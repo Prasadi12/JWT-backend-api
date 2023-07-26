3271b5345007113012ce106dc2e2ee6dd5baaec5
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-26 15:59:43 +0530

    api

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "signup"
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "signup",
+});
 
-app.post('/signup', (req,res)=>{
-    const sql = 'INSERT INTO registration (`email`,`password`) VALUES(?)';
-    const value = [
-        req.body.email,
-        req.body.password
-    ]
-    db.query(sql, [value], (err, data)=>{
-        if(err){
-            res.json('Error')
-        }
-        return res.json(data)
-    })
-})
+app.post("/signup", (req, res) => {
+  const sql = "INSERT INTO registration (`email`,`password`) VALUES(?)";
+  const value = [req.body.email, req.body.password];
+  db.query(sql, [value], (err, data) => {
+    if (err) {
+      res.json("Error");
+    }
+    return res.json(data);
+  });
+});
 
-app.post('/login', (req,res)=>{
-    const sql = "SELECT * FROM registration WHERE `email` = ? AND `password` = ?";
-    db.query(sql, [req.body.email,req.body.password], (err,data)=>{
-        if(err){
-            res.json('Error')
-        }
-        else if(data.length > 0){
-            return res.json('Success')
-        }else{
-            return res.json('Fail')
-        }
-    })
-})
+app.post("/login", (req, res) => {
+  const sql = "SELECT * FROM registration WHERE `email` = ? AND `password` = ?";
+  db.query(sql, [req.body.email, req.body.password], (err, data) => {
+    if (err) {
+      res.json("Error");
+    } else if (data.length > 0) {
+      return res.json("Success");
+    } else {
+      return res.json("Fail");
+    }
+  });
+});
 
-app.listen(5000, ()=>{
-    console.log('NodeApi is running on port 5000')
-})
+app.listen(5000, () => {
+  console.log("NodeApi is running on port 5000");
+});
